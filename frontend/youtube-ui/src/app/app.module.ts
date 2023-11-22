57a4e3f1c0b27f6f070ccb81528ea40354a57994
MA514R7UBY <marcoslopezsanchez@hotmail.com>
2023-11-22 13:43:09 -0600

    Summary!
    Change version ng 16.2.12

---
+++ frontend/youtube-ui/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UploadVideoComponent } from './upload-video/upload-video.component';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
+import { NgxFileDropModule } from 'ngx-file-drop';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UploadVideoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    NgxFileDropModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
